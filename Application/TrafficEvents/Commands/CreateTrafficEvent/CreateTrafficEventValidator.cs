dc3bf288b7e7e7bddb62cc9b079b660db946d77b
Mohammad Al-Bourm <mohammadbourm@gmail.com>
2024-05-17 23:16:02 +0400

    Add Validation To Create Traffic Event Command

--- Application/TrafficEvents/Commands/CreateTrafficEvent/CreateTrafficEventValidator.cs
+++ Application/TrafficEvents/Commands/CreateTrafficEvent/CreateTrafficEventValidator.cs
@@ -6,6 +6,31 @@ internal sealed class CreateTrafficEventValidator : AbstractValidator<CreateTraf
 {
     public CreateTrafficEventValidator()
     {
+        RuleFor(x => x.SessionID)
+                .NotEmpty().WithMessage("SessionID is required.")
+                .Length(1, 50).WithMessage("SessionID must be between 1 and 50 characters.");
+
+        RuleFor(x => x.Time)
+            .LessThanOrEqualTo(DateTime.Now).WithMessage("Time cannot be in the future.");
+
+        RuleFor(x => x.Number)
+            .GreaterThanOrEqualTo(0).WithMessage("Number must be zero or greater.");
+
+        RuleFor(x => x.Category)
+            .NotEmpty().WithMessage("Category is required.")
+            .MaximumLength(100).WithMessage("Category cannot be longer than 100 characters.");
+
+        RuleFor(x => x.LaneId)
+            .GreaterThan(0).WithMessage("LaneId must be greater than zero.");
+
+        RuleFor(x => x.Other)
+            .MaximumLength(200).WithMessage("Other cannot be longer than 200 characters.");
+
+        RuleFor(x => x.Value)
+            .GreaterThanOrEqualTo(0).WithMessage("Value must be zero or greater.");
+
+        RuleFor(x => x.Direction)
+            .InclusiveBetween(0, 360).WithMessage("Direction must be between 0 and 360 degrees.");
 
     }
 }
